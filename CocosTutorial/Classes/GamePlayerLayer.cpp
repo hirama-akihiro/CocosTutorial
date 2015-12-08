909f083ff9581bdbd551deaee51e5e0820cc67b2
showyou <hirama.showyou@gmail.com>
2015-12-09 03:25:20 +0900

    チュートリアル第４回時点でのプロジェクト

--- CocosTutorial/Classes/GamePlayerLayer.cpp
+++ CocosTutorial/Classes/GamePlayerLayer.cpp
@@ -24,6 +24,41 @@ bool GamePlayerLayer::init()
     // 1. 最初に親クラスの初期化を行う
     if ( !Layer::init() ){ return false; }
     
+    // 2. スプライトフレームキャッシュを作成する
+    auto cacher = SpriteFrameCache::getInstance();
+    
+    // 3. plistを読み込む
+    cacher->addSpriteFramesWithFile("running.plist");
+    auto spriteRunner = Sprite::create();
+    
+    // 4. 主人公の位置を設定する
+    spriteRunner->setPosition(Vec2(80,85));
+    
+    // 5. 移動アクションを作成する
+    // 画像を入れつに読み込む
+    Vector<SpriteFrame*> frames;
+    for(int i = 0; i < 8; i++){
+        std::stringstream ss;
+        ss << "runner" << i << ".png";
+        CCLOG("%d¥n",0);
+        frames.pushBack(cacher->getSpriteFrameByName(ss.str()));
+    }
+    
+    // 6. アニメーションを作成する
+    auto anim = Animation::createWithSpriteFrames(frames, 0.1f);
+    
+    // 7. アニメーションアクションを作成する
+    auto action = Animate::create(anim);
+    
+    // 8. 永久に繰り返すアクションを作成する
+    auto anime = RepeatForever::create(action);
+    
+    // アクションを実行する
+    spriteRunner->runAction(anime);
+    
+    addChild(spriteRunner);
+    
+    /*
     // 2. 主人公のスプライトを作成する
     auto spriteRunner = Sprite::create("runner.png");
     
@@ -34,6 +69,7 @@ bool GamePlayerLayer::init()
     auto actionTo = MoveTo::create(2, Vec2(300,85));
     spriteRunner->runAction(actionTo);
     addChild(spriteRunner);
+     */
     
     return true;
 }
